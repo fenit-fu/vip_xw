d768aacb4e0accd3ddc4bc889263adf83b75a8e8
gongmwgit <1451596779@qq.com>
2019-02-23 22:17:51 +0800

    fix(url): add url

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 require('express-async-errors')
 const rp = require('request-promise')
+const _ = require('lodash')
 const config = require('config')
 const carouseModel = require('../mongoose').carouse
 
@@ -12,17 +13,34 @@ class IndexController {
   async getNewMovieList(req, res) {
     const type = req.query.type || 'ZX'
     const options = {
-      url: `${config.get('reptileZxMovieUrl.wangzherongyao')}${type}`,
+      url: `${config.get('reptileZxMovieUrl.wangzherongyao.list')}${type}`,
       method: 'GET',
       json: true
     }
-    res.sendOk(await rp(options))
+    const result = await rp(options)
+    const filterResult = _.filter(result, (item) => {
+      return (item.url).indexOf('http://v.yimohui2017.com') === -1
+          && (item.url).indexOf('http://v.sanyiwangluo.com') === -1
+          && (item.url).indexOf('v.lyerhuo.com') === -1
+          && (item.url).indexOf('v.ynkqx.com') === -1
+          && (item.url).indexOf('v.zhubodasai.com') === -1
+          && (item.url).indexOf('v.aimushang.com') === -1
+          && (item.url).indexOf('v.shinegobaby.com') === -1
+    })
+    res.sendOk(filterResult)
+    // res.sendOk(_.map(filterResult, function (item) {
+    //   return {
+    //     uuid: item.uuid,
+    //     name: item.name,
+    //     url: item.url
+    //   }
+    // }))
   }
 
   async getMovieListByName(req, res) {
-    const name = (req.query.name).trim() || 'ZX'
+    const name = (req.query.name)
     const options = {
-      url: `${config.get('reptileZxMovieUrl.wangzherongyaoSearch')}` + encodeURIComponent(name),
+      url: name,
       method: 'GET',
       json: true
     }
@@ -34,6 +52,7 @@ class IndexController {
 
     try {
       const filmOptions = {
+        //url: 'https://acfun.iqiyi-kuyun.com/20190208/HRcU03fW/index.m3u8',
         url: `${config.get('reptileZxMovieUrl.wangzherongyao.filmDetail')}` + id + '?uuid=' + id,
         method: 'GET',
         json: true
--- extend/response.js
+++ extend/response.js
@@ -2,6 +2,11 @@ const lodash = require('lodash')
 module.exports = function (req, res, next) {
   const unifyResult = {
     sendOk: function (data) {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+      res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+      res.header('X-Powered-By', ' 3.2.1')
+      res.header('Content-Type', 'application/json;charset=utf-8')
       return res.send(lodash.extend({
         code: 0,
         msg: 'ok',
@@ -11,8 +16,8 @@ module.exports = function (req, res, next) {
     setAttachment: function (fileName) {
       res.setHeader('Content-Type', 'application/octet-stream')
       res.setHeader(
-        'Content-Disposition',
-        'attachment; filename=rank_' + fileName + '.xlsx'
+          'Content-Disposition',
+          'attachment; filename=rank_' + fileName + '.xlsx'
       )
     }
   }
