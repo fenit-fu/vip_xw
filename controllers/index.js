073ed82c897dcf7003df1b188e53b4575207e295
gongmw <gongmwgit@myoffer.com>
2019-02-28 10:13:53 +0800

    feat(filter):  fial movie

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,8 @@ const rp = require('request-promise')
 const _ = require('lodash')
 const config = require('config')
 const carouseModel = require('../mongoose').carouse
+const NEW_MOVIES = require('../constants/newmovies')
+const serviceIndex = require('../service/index')
 
 class IndexController {
   async getCarouselList(req, res) {
@@ -11,70 +13,7 @@ class IndexController {
   }
 
   async getCinemasMovies(req, res) {
-    res.sendOk([{
-      'uuid': '1',
-      'name': '战斗天使',
-      'classify': '科幻片',
-      'director': '罗伯特·罗德里格兹',
-      'starring': '詹姆斯·卡梅隆、莱塔·卡罗格里迪斯',
-      'releaseDate': '2019-02-23',
-      'cover': 'https://img.xiguaimg.com/pic/uploadimg/2018-12/20181271134064772.jpg',
-      'intro': '阿丽塔：战斗天使》根据日本漫画家木城雪户的代表作《铳梦》改编，讲述了末世时代，半人半机械的生理架构已经不足为奇。某天倒在废墟中仅剩头颅的阿丽塔被艾德医生捡起，以机械为血肉重见人间。但苏醒后她完全丧失了记忆，对自己的身世一无所知',
-      'url': 'http://bilibili.com-l-bilibili.com/20190223/8039_6a310f25/1000k/hls/index.m3u8',
-    }, {
-      'uuid': '2',
-      'name': '一吻定情2019',
-      'classify': '喜剧',
-      'director': '陈玉珊',
-      'starring': '王大陆 林允 陈柏融 蔡思韵 邰智源 ',
-      'releaseDate': '2019-02-15',
-      'cover': 'https://img.kuyun88.com/pic/uploadimg/2019-2/201921512591130960.jpg',
-      'intro': '笨蛋爱上天才，会有结果吗？平凡女孩原湘琴（林允饰）喜欢上了天才少年江直树（王大陆饰），在她表白失败准备放弃之际，爸爸居然带着自己搬进了直树家里？！一个猛追，一个猛逃',
-      'url': 'https://acfun.iqiyi-kuyun.com/ppvod/6CF94FDBE9BB49B83FA30FD679F76E46.m3u8',
-    }, {
-      'uuid': '3',
-      'name': '流浪地球',
-      'classify': '科幻片',
-      'director': '郭帆',
-      'starring': '屈楚萧,吴京,李',
-      'releaseDate': '2019-02-08',
-      'cover': 'http://pic.zuikzy.com/upload/vod/2019-02-06/15494446320.jpg',
-      'intro': '太阳即将毁灭，人类在地球表面建造出巨大的推进器，寻找新家园。然而宇宙之路危机四伏，为了拯救地球，为了人类能在漫长的2500年后抵达新的家园，流浪地球时代的年轻人挺身而出，展开争分夺秒的生死之战。',
-      'url': 'https://acfun.iqiyi-kuyun.com/ppvod/CEA373893E753431106BE1FE32BF5054.m3u8'
-    }, {
-      'uuid': '4',
-      'name': '疯狂的外星人',
-      'classify': '喜剧片',
-      'director': '宁浩',
-      'starring': '黄渤,沈腾,马修',
-      'releaseDate': '2019-02-08',
-      'cover': 'https://img.xiguaimg.com/pic/uploadimg/2019-2/2019273335484116.jpg',
-      'tag': '高清',
-      'intro': '耿浩（黄渤 饰）与一心想发大财的好兄弟大飞（沈腾 饰），经营着各自惨淡的“事业”，然而“天外来客”的意外降临，打破了二人平静又拮据的生活。神秘的西方力量也派出“哼哈二将”在全球搜查外星人行踪。啼笑皆非的跨物种对决，别开生面的“星战”，在中国某海边城市激情上演。',
-      'url': 'http://youku.com-l-youku.com/20190207/20333_87dc3583/1000k/hls/index.m3u8'
-    }, {
-      'uuid': '5',
-      'name': '神探蒲松龄',
-      'classify': '喜剧片',
-      'director': '严嘉',
-      'starring': '成龙,阮经天,钟',
-      'releaseDate': '2019-02-08',
-      'cover': 'http://pic.zuikzy.com/upload/vod/2019-02-06/15494143326.jpg',
-      'tag': '高清',
-      'intro': '一代文豪蒲松龄（成龙饰）执阴阳判化身神探，与捕快严飞（林柏宏饰）联手追踪金华镇少女失踪案。蒲松龄带领“猪狮虎”“屁屁”“忘忧”“千手”等一众小妖深入案情，在找寻真相的过程中，牵扯出一段旷世奇恋。',
-      'url': 'http://bilibili.com-l-bilibili.com/20190207/8035_af0c52a1/1000k/hls/index.m3u8'
-    }, {
-      'uuid': '6',
-      'name': '飞驰人生',
-      'classify': '喜剧片',
-      'director': '韩寒',
-      'starring': '沈腾,黄景瑜,尹',
-      'releaseDate': '2019-02-08',
-      'cover': 'http://img.kuyun88.com/pic/uploadimg/2019-2/20192423202435417.jpg',
-      'tag': '高清',
-      'intro': '曾经在赛车界叱咤风云,如今却只能经营炒饭大排档的赛车手张驰（沈腾饰）决定重返车坛挑战年轻一代的天才。然而没钱没车没队友，甚至驾照都得重新考，这场笑料百出不断被打脸的复出之路，还有更多哭笑不得的窘境在等待着这位过气车神……',
-      'url': 'http://youku.com-l-youku.com/20190207/20332_cdc7792b/1000k/hls/index.m3u8'
-    }])
+    res.sendOk(NEW_MOVIES.MANUAL)
   }
 
   async getNewMovieList(req, res) {
@@ -93,15 +32,9 @@ class IndexController {
         && (item.url).indexOf('v.zhubodasai.com') === -1
         && (item.url).indexOf('v.aimushang.com') === -1
         && (item.url).indexOf('v.shinegobaby.com') === -1
+        && (item.url).indexOf('v.tian1886.com') === -1
     })
     res.sendOk(filterResult)
-    // res.sendOk(_.map(filterResult, function (item) {
-    //   return {
-    //     uuid: item.uuid,
-    //     name: item.name,
-    //     url: item.url
-    //   }
-    // }))
   }
 
   async getMovieListByName(req, res) {
@@ -117,131 +50,8 @@ class IndexController {
 
   async getMovieDetailByUuid(req, res) {
     const id = (req.query.uuid).trim()
-    if (id === '1') {
-      return res.sendOk({
-        'data': {
-          'uuid': '1',
-          'name': '战斗天使',
-          'classify': '科幻片',
-          'director': '罗伯特·罗德里格兹',
-          'starring': '詹姆斯·卡梅隆、莱塔·卡罗格里迪斯',
-          'releaseDate': '2019-02-23',
-          'cover': 'https://img.xiguaimg.com/pic/uploadimg/2018-12/20181271134064772.jpg',
-          'intro': '阿丽塔：战斗天使》根据日本漫画家木城雪户的代表作《铳梦》改编，讲述了末世时代，半人半机械的生理架构已经不足为奇。某天倒在废墟中仅剩头颅的阿丽塔被艾德医生捡起，以机械为血肉重见人间。但苏醒后她完全丧失了记忆，对自己的身世一无所知',
-          'url': 'http://bilibili.com-l-bilibili.com/20190223/8039_6a310f25/1000k/hls/index.m3u8',
-        }, 'type': 'film'
-      })
-    }
-    if (id === '2')
-      return res.sendOk({
-          'data': {
-            'uuid': '2',
-            'name': '一吻定情2019',
-            'classify': '喜剧',
-            'director': '陈玉珊',
-            'starring': '王大陆 林允 陈柏融 蔡思韵 邰智源 ',
-            'releaseDate': '2019-02-15',
-            'cover': 'https://img.kuyun88.com/pic/uploadimg/2019-2/201921512591130960.jpg',
-            'intro': '笨蛋爱上天才，会有结果吗？平凡女孩原湘琴（林允饰）喜欢上了天才少年江直树（王大陆饰），在她表白失败准备放弃之际，爸爸居然带着自己搬进了直树家里？！一个猛追，一个猛逃',
-            'url': 'https://acfun.iqiyi-kuyun.com/ppvod/6CF94FDBE9BB49B83FA30FD679F76E46.m3u8',
-          },
-          'type': 'film'
-        }
-      )
-    if (id === '3')
-      return res.sendOk({
-        'data': {
-          'uuid': '3',
-          'name': '流浪地球',
-          'classify': '科幻片',
-          'director': '郭帆',
-          'starring': '屈楚萧,吴京,李',
-          'releaseDate': '2019-02-08',
-          'cover': 'http://pic.zuikzy.com/upload/vod/2019-02-06/15494446320.jpg',
-          'intro': '太阳即将毁灭，人类在地球表面建造出巨大的推进器，寻找新家园。然而宇宙之路危机四伏，为了拯救地球，为了人类能在漫长的2500年后抵达新的家园，流浪地球时代的年轻人挺身而出，展开争分夺秒的生死之战。',
-          'url': 'https://acfun.iqiyi-kuyun.com/ppvod/CEA373893E753431106BE1FE32BF5054.m3u8'
-        }, 'type': 'film'
-      })
-    if (id === '4')
-      return res.sendOk({
-        'data': {
-          'uuid': '4',
-          'name': '疯狂的外星人',
-          'classify': '喜剧片',
-          'director': '宁浩',
-          'starring': '黄渤,沈腾,马修',
-          'releaseDate': '2019-02-08',
-          'cover': 'https://img.xiguaimg.com/pic/uploadimg/2019-2/2019273335484116.jpg',
-          'tag': '高清',
-          'intro': '耿浩（黄渤 饰）与一心想发大财的好兄弟大飞（沈腾 饰），经营着各自惨淡的“事业”，然而“天外来客”的意外降临，打破了二人平静又拮据的生活。神秘的西方力量也派出“哼哈二将”在全球搜查外星人行踪。啼笑皆非的跨物种对决，别开生面的“星战”，在中国某海边城市激情上演。',
-          'url': 'http://youku.com-l-youku.com/20190207/20333_87dc3583/1000k/hls/index.m3u8'
-        }, 'type': 'film'
-      })
-    if (id === '5')
-      return res.sendOk({
-        'data': {
-          'uuid': '5',
-          'name': '神探蒲松龄',
-          'classify': '喜剧片',
-          'director': '严嘉',
-          'starring': '成龙,阮经天,钟',
-          'releaseDate': '2019-02-08',
-          'cover': 'http://pic.zuikzy.com/upload/vod/2019-02-06/15494143326.jpg',
-          'tag': '高清',
-          'intro': '一代文豪蒲松龄（成龙饰）执阴阳判化身神探，与捕快严飞（林柏宏饰）联手追踪金华镇少女失踪案。蒲松龄带领“猪狮虎”“屁屁”“忘忧”“千手”等一众小妖深入案情，在找寻真相的过程中，牵扯出一段旷世奇恋。',
-          'url': 'http://bilibili.com-l-bilibili.com/20190207/8035_af0c52a1/1000k/hls/index.m3u8'
-        }, 'type': 'film'
-      })
-    if (id === '6')
-      return res.sendOk({
-        'data': {
-          'uuid': '6',
-          'name': '飞驰人生',
-          'classify': '喜剧片',
-          'director': '韩寒',
-          'starring': '沈腾,黄景瑜,尹',
-          'releaseDate': '2019-02-08',
-          'cover': 'http://img.kuyun88.com/pic/uploadimg/2019-2/20192423202435417.jpg',
-          'tag': '高清',
-          'intro': '曾经在赛车界叱咤风云,如今却只能经营炒饭大排档的赛车手张驰（沈腾饰）决定重返车坛挑战年轻一代的天才。然而没钱没车没队友，甚至驾照都得重新考，这场笑料百出不断被打脸的复出之路，还有更多哭笑不得的窘境在等待着这位过气车神……',
-          'url': 'http://youku.com-l-youku.com/20190207/20332_cdc7792b/1000k/hls/index.m3u8'
-        }, 'type': 'film'
-      })
-    try {
-      const filmOptions = {
-        url: `${config.get('reptileZxMovieUrl.wangzherongyao.filmDetail')}` + id + '?uuid=' + id,
-        method: 'GET',
-        json: true
-      }
-      const filmResult = await rp(filmOptions)
-      if ((filmResult.url).indexOf('http://v.yimohui2017.com') === -1
-        && (filmResult.url).indexOf('http://v.sanyiwangluo.com') === -1
-        && (filmResult.url).indexOf('v.lyerhuo.com') === -1
-        && (filmResult.url).indexOf('v.ynkqx.com') === -1
-        && (filmResult.url).indexOf('v.zhubodasai.com') === -1
-        && (filmResult.url).indexOf('v.aimushang.com') === -1
-        && (filmResult.url).indexOf('v.shinegobaby.com') === -1) {
-        return res.sendOk({
-          data: filmResult,
-          type: 'film'
-        })
-      }
-      else {
-        return res.sendOk('此视频以后台记录保存即将上架')
-      }
-    }
-    catch (e) {
-      const teleplayOptions = {
-        url: `${config.get('reptileZxMovieUrl.wangzherongyao.teleplayDetail')}` + id + '?uuid=' + id,
-        method: 'GET',
-        json: true
-      }
-      const teleplayResult = await rp(teleplayOptions)
-      return res.sendOk({
-        data: teleplayResult,
-        type: 'teleplay'
-      })
-    }
+    const result = await serviceIndex.getMovieDetailByUuid(id)
+    res.sendOk(result)
   }
 }
 
--- service/index.js
+++ service/index.js
@@ -1,7 +1,91 @@
-const helper = require('../extend/helper')
+const config = require('config')
+const _ = require('lodash')
+const rp = require('request-promise')
+const NEW_MOVIES = require('../constants/newmovies')
 
-class CityService {
+class HomeService {
+  async getMovieDetailByUuid(uuid) {
+    if (uuid === '1')
+      return {
+        data: NEW_MOVIES.MANUAL[0],
+        type: 'film'
+      }
+    if (uuid === '2')
+      return {
+        data: NEW_MOVIES.MANUAL[1],
+        type: 'film'
+      }
+    if (uuid === '3')
+      return {
+        data: NEW_MOVIES.MANUAL[2],
+        type: 'film'
+      }
+    if (uuid === '4')
+      return {
+        data: NEW_MOVIES.MANUAL[3],
+        type: 'film'
+      }
+    if (uuid === '5')
+      return {
+        data: NEW_MOVIES.MANUAL[4],
+        type: 'film'
+      }
+    if (uuid === '6')
+      return {
+        data: NEW_MOVIES.MANUAL[5],
+        type: 'film'
+      }
+    try {
+      const filmOptions = {
+        url: `${config.get('reptileZxMovieUrl.wangzherongyao.filmDetail')}` + uuid + '?uuid=' + uuid,
+        method: 'GET',
+        json: true
+      }
+      const filmResult = await rp(filmOptions)
+      if ((filmResult.url).indexOf('http://v.yimohui2017.com') === -1
+        && (filmResult.url).indexOf('http://v.sanyiwangluo.com') === -1
+        && (filmResult.url).indexOf('v.lyerhuo.com') === -1
+        && (filmResult.url).indexOf('v.ynkqx.com') === -1
+        && (filmResult.url).indexOf('v.zhubodasai.com') === -1
+        && (filmResult.url).indexOf('v.aimushang.com') === -1
+        && (filmResult.url).indexOf('v.shinegobaby.com') === -1
+        && (filmResult.url).indexOf('v.tian1886.com') === -1) {
+        return {
+          data: filmResult,
+          type: 'film'
+        }
+      }
+      else {
+        return '此视频以后台记录保存即将上架'
+      }
+    }
+    catch (e) {
+      const teleplayOptions = {
+        url: `${config.get('reptileZxMovieUrl.wangzherongyao.teleplayDetail')}` + uuid + '?uuid=' + uuid,
+        method: 'GET',
+        json: true
+      }
+      const teleplayResult = await rp(teleplayOptions)
 
+      teleplayResult.teleplayLinkDtoList = _.filter(teleplayResult.teleplayLinkDtoList, (item) => {
+        return (item.url).indexOf('v.yimohui2017.com') === -1
+          && (item.url).indexOf('v.sanyiwangluo.com') === -1
+          && (item.url).indexOf('v.lyerhuo.com') === -1
+          && (item.url).indexOf('v.ynkqx.com') === -1
+          && (item.url).indexOf('v.zhubodasai.com') === -1
+          && (item.url).indexOf('v.aimushang.com') === -1
+          && (item.url).indexOf('v.shinegobaby.com') === -1
+          && (item.url).indexOf('v.tian1886.com') === -1
+          && (item.url).indexOf('v2.aimushang.com') === -1
+          && (item.url).indexOf('v.gmitking.com') === -1
+      })
+
+      return {
+        data: teleplayResult,
+        type: 'teleplay'
+      }
+    }
+  }
 }
 
-module.exports = new CityService()
+module.exports = new HomeService()
