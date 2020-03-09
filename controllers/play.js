059004395479761fd60d0c3346f85f6853704668
gongmw <15549565531@163.com>
2020-03-09 13:52:00 +0800

    fix: querything length

--- controllers/play.js
+++ controllers/play.js
@@ -1,10 +1,9 @@
 require('express-async-errors')
-const playService = require('../service/play')
 const querystring = require('querystring')
 
 class PlayController {
   async getTrueUrl(req, res) {
-    res.sendOk(decodeURIComponent(querystring.stringify(req.query)))
+    res.sendOk(decodeURIComponent(querystring.stringify(req.query)).substring(5))
   }
 }
 
